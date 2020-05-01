af462cad5db53f889554402ce9c7777775fd4b85
mb <mb_wali@hotmail.com>
2020-05-01 19:29:45 +0200

    tests modify

--- invenio_theme_tugraz/webpack.py
+++ invenio_theme_tugraz/webpack.py
@@ -2,8 +2,7 @@
 #
 # Copyright (C) 2020 TUGRAZ.
 #
-# invenio-theme-tugraz  is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# invenio-theme-tugraz  is free software.
 
 """JS/CSS Webpack bundles for theme."""
 
