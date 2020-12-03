84f963b40762d8f429af9e9d16e1121288bbcd0f
Mojib Wali <44528277+mb-wali@users.noreply.github.com>
2020-12-03 15:19:45 +0100

    ci(repo): migrate to github actions. (#119)
    tests(repo): modified to github actions.

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,9 @@
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
-docker-services-cli up postgresql es redis
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-docker-services-cli up es postgresql redis
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli --verbose up es postgresql redis
 python -m pytest
 tests_exit_code=$?
 docker-services-cli down
