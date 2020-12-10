204f7b81c3d38228790b73e8a9af254c0c570733
Mojib Wali <44528277+mb-wali@users.noreply.github.com>
2020-12-10 12:25:52 +0100

    test: adding tests
    * test: removed no cover
    * tests: adding tests for functions.
       * test_make_dict_like
       * test_cast_to_dict

--- setup.py
+++ setup.py
@@ -17,8 +17,9 @@ history = open("CHANGES.rst").read()
 
 tests_require = [
     "pytest-invenio>=1.4.0",
+    'invenio-app>=1.3.0,<2.0.0',
     # TODO: remove once a new release is out
-    'docker-services-cli>=0.2.1,<0.3.0',
+    'docker-services-cli>=0.2.1,<0.3.0'
 ]
 
 extras_require = {
