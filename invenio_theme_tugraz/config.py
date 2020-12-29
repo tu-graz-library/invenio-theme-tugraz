976564e331cfa853b0a61fad2e3d2517fbd979bf
Christoph Ladurner <christoph.ladurner@tugraz.at>
2020-12-29 15:53:50 +0100

    bugfix(config): add source of SEARCH_UI_SEARCH_TEMPLATE variable

--- invenio_theme_tugraz/config.py
+++ invenio_theme_tugraz/config.py
@@ -95,5 +95,8 @@ DEPOSITS_HEADER_TEMPLATE = "invenio_theme_tugraz/header.html"
 # )
 """override the default record landing page"""
 
+# Invenio-search-ui
+# =============
+# See https://invenio-search-ui.readthedocs.io/en/latest/configuration.html
 SEARCH_UI_SEARCH_TEMPLATE = "invenio_theme_tugraz/search.html"
 """override the default search page"""
