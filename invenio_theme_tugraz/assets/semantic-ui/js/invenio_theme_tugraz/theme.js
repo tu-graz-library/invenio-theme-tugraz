1649dfad09f8dedd86eb30f859334f6a6dfe05e5
David Eckhard <dabombeeer@gmail.com>
2021-03-02 13:46:39 +0100

    feature(ui): add zammad contact form

--- invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
+++ invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
@@ -1,3 +1,14 @@
+import { jQuery, $ } from 'jquery';
+
+$(function() {
+  $('#feedback-form').ZammadForm({
+    messageTitle: 'Feedback Form',
+    messageSubmit: 'Send',
+    messageThankYou: 'Thank you for your message, (#%s). We will get back to as quickly as possible!',
+    modal: true
+  });
+});
+
 export function toggleVisibility(id) {
     var element = document.getElementById(id);
     var isHided = element.style.display === "none";
