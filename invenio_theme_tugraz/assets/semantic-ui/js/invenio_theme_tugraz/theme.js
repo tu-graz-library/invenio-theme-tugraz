baa7d290c726a1079aee645f7a04827972652b1f
rekt-hard <72449192+rekt-hard@users.noreply.github.com>
2021-03-09 09:04:32 +0100

    zammad contact form (#156)

--- invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
+++ invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
@@ -1,3 +1,21 @@
+import $ from 'jquery';
+
+$(function() {
+  let scriptNode = document.createElement("hidden"); //needed for zammad script
+  scriptNode.id = "zammad_form_script";
+  scriptNode.src = "https://ub-support.tugraz.at/assets/form/form.js";
+  document.head.appendChild(scriptNode);
+  
+  $.getScript("https://ub-support.tugraz.at/assets/form/form.js", () => {
+    $('#feedback-form').ZammadForm({
+      messageTitle: 'Feedback Form',
+      messageSubmit: 'Send',
+      messageThankYou: 'Thank you for your message, (#%s). We will get back to you as quickly as possible!',
+      modal: true
+    });
+  });
+});
+
 export function toggleVisibility(id) {
     var element = document.getElementById(id);
     var isHided = element.style.display === "none";
