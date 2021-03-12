6210a8bad925f31a04551b0cf87b8ad0955bf900
Unix <mb_wali@hotmail.com>
2021-03-12 10:27:43 +0100

    global: adds copyrights

--- requirements-devel.txt
+++ requirements-devel.txt
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 mojib wali.
+# Copyright (C) 2020-2021 Graz University of Technology.
 #
 # invenio-theme-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2019-2020 CERN.
 # Copyright (C) 2019-2020 Northwestern University.
-# Copyright (C) 2020 Graz University of Technology.
+# Copyright (C) 2020-2021 Graz University of Technology.
 #
 # invenio-theme-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
