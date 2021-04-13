0df54a83f47107085f9ed4f4127e36d002bfe331
david <dabombeeer@gmail.com>
2021-04-13 11:58:45 +0200

    fix: blur link on click, clickable login with TUG button

--- invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
+++ invenio_theme_tugraz/assets/semantic-ui/js/invenio_theme_tugraz/theme.js
@@ -1,7 +1,24 @@
 import $ from 'jquery';
 import 'semantic-ui-css';
 
+let lastClickedElement = null;
+
+// called on document ready
 $(function() {
+
+  // remove focus on click for outgoing links to reset css
+  // store clicked element so tab index can be restored
+  $('a').on('click', (eventHandler) => {
+    let target = eventHandler.target;
+    lastClickedElement = target;
+    target.blur();
+  })
+
+  $(document).on('keydown', keyDownHandler);
+  importZammadScript();
+});
+
+function importZammadScript() {
   let scriptNode = document.createElement("hidden"); //needed for zammad script
   scriptNode.id = "zammad_form_script";
   scriptNode.src = "https://ub-support.tugraz.at/assets/form/form.js";
@@ -16,7 +33,22 @@ $(function() {
       modal: true
     });
   });
-});
+}
+
+function keyDownHandler(keyDownEvent) {
+  switch (keyDownEvent.key) {
+    case 'Tab':
+      // restore activeElement after clicking on outgoing link
+      if (lastClickedElement !== null) {
+        lastClickedElement.focus();
+        lastClickedElement = null;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
 
 // used for sticky test instance notification
 $('.ui.sticky.test-instance')
