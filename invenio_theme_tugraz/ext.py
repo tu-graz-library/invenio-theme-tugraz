d1b682ef3e4345226eea6797488bc70218bb6324
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-05-26 12:44:57 +0200

    fix: add logger output
    * without no information would be shown in the production instance

--- invenio_theme_tugraz/ext.py
+++ invenio_theme_tugraz/ext.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2024 Graz University of Technology.
+# Copyright (C) 2020-2025 Graz University of Technology.
 #
 # invenio-theme-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
